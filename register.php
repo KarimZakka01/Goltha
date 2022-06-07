f796cba5c6162db796f934cf8462888f4dff6373
Karim Zakka <karimzakka2001@gmail.com>
2022-06-07 18:07:37 +0300

    register page navbar and js script

--- register.php
+++ register.php
@@ -69,6 +69,30 @@ wght@100;200;300;400;600;700&display=swap" rel="stylesheet">
 <body class = "regpage">
 
 
+<nav>
+            <a href="index.html"><img src="images/golthalogo.svg" alt="This is the logo"></a>
+            <div class = "navigation" id="navLinks">
+            <i class="fa fa-times" onclick="hideMenu()"></i>
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="services.php">Services</a></li>
+                    <li><a href="contact.php">Contact</a></li>
+                    <!-- <li><a href="#">More</a>
+                    <ul> -->
+                    <li><a href="register.php">Register</a></li>
+                    <li><a href="logout.php">Logout</a></li>
+                    <!-- </ul>
+                    </li> -->
+                    
+                    
+                </ul>
+            </div>
+            <i class="fa fa-bars" onclick="showMenu()"></i> <!--menu icon from font awesome-->
+          
+        </nav>
+
+
     <!-------REGISTER CONTENT------->
 <div class="mainbox">
    <div class="content">
@@ -122,7 +146,19 @@ wght@100;200;300;400;600;700&display=swap" rel="stylesheet">
 </section>
 
     
+<script>
+    var navLinks = document.getElementById("navLinks");
 
+    function showMenu(){
+        navLinks.style.right = "0";
+    
+    }
+    function hideMenu(){
+        navLinks.style.right = "-200px";
+    
+    }
+
+</script>
 
    
 </body>
